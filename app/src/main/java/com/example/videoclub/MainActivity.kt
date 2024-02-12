74949c3afbf825863e6f2a9a3ed02b21e776bd6a
MrSixtorz <sixrod723@gmail.com>
2024-02-12 13:15:51 +0100

    Pequeños requisitos

--- app/src/main/java/com/example/videoclub/MainActivity.kt
+++ app/src/main/java/com/example/videoclub/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.Firebase
