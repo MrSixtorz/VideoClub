aeb7540474098db98b865d77a06488dd63435f32
MrSixtorz <sixto723@gmail.com>
2024-02-14 21:28:47 +0100

    chorradas

--- app/src/main/java/com/example/videoclub/Result.kt
+++ app/src/main/java/com/example/videoclub/Result.kt
@@ -1,18 +1,12 @@
 package com.example.videoclub
 
+import com.google.gson.annotations.SerializedName
+
 data class Result(
-    val adult: Boolean,
-    val backdrop_path: String,
-    val genre_ids: List<Int>,
-    val id: Int,
-    val original_language: String,
-    val original_title: String,
-    val overview: String,
-    val popularity: Double,
-    val poster_path: String,
-    val release_date: String,
-    val title: String,
-    val video: Boolean,
-    val vote_average: Double,
-    val vote_count: Int
+    @SerializedName("genre_ids") val genre_ids: List<Int>,
+    @SerializedName("id") val id: Int,
+    @SerializedName("overview") val overview: String,
+    @SerializedName("poster_path") val poster_path: String,
+    @SerializedName("release_date") val release_date: String,
+    @SerializedName("title") val title: String
 )
